57069b516206ae3a284ef855963cdc62a615b84c
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-03-04 16:41:29 +0000

    save

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,12 @@
+/*
+File: Converting Data Types
+Author: Stephen Li
+Date Created: Mar 4, 2026
+Date Last Modified: Mar 4, 2026
+*/
+
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -8,19 +17,37 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		int num1 = input.nextInt();
+		int num2 = (int)num1;
+		System.out.println(num2 + 3);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		int num1 = input.nextInt();
+		String word1 = num1 + "4";
+		double num2 = Double.parseDouble(word1);
+		System.out.println(num2 + 2);
+		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a radius: ");
+		double num1 = input.nextDouble();
+		double num2 = (double)num1;
+		System.out.println((num2 * num2) * 3.14);
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		double num1 = input.nextDouble();
+		num1 = (Math.floor((num1 * 12)));
+		System.out.println(num1);
 	}
 
 }
